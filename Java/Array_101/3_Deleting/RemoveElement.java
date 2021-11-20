905be5a5dd73a97f9317513a5c69a65b8b4b6422
davidoh919 <doh24@wisc.edu>
2021-11-20 00:07:35 -0600

    edit Remove Element

--- Java/Array_101/3_Deleting/RemoveElement.java
+++ Java/Array_101/3_Deleting/RemoveElement.java
@@ -2,18 +2,17 @@ public class RemoveElement {
     class Solution {
         public int removeElement(int[] nums, int val) {
             int n = nums.length;
-            int ret = n;
             int t;
             for (int i = 0; i < n; i++) {
                 if (nums[i] == val) {
-                    for (int j = n - 1; j > i; j--) {
-                        nums[j - 1] = nums[j];
+                    for (int j = i; j < n - 1; j++) {
+                        nums[j] = nums[j + 1];
                     }
-                    i++;
-                    ret--;
+                    n--;
+                    i--;
                 }
             }
-            return ret;
+            return n;
         }
     }
 }
