d1ce80dc46e1db65ee76b001d77f2bb0f129bf4f
davidoh919 <doh24@wisc.edu>
2021-12-02 20:14:35 -0600

    add HeightChecker

---
+++ Java/Array_101/6_Others/HeightChecker.java
@@ -0,0 +1,16 @@
+public class HeightChecker {
+    class Solution {
+        public int heightChecker(int[] heights) {
+            int ans = 0;
+            int n = heights.length;
+            int expected[] = new int[n];
+            for (int i = 0; i < n; i++)
+                expected[i] = heights[i];
+            Arrays.sort(expected);
+            for (int i = 0; i < n; i++)
+                if (heights[i] != expected[i])
+                    ans++;
+            return ans;
+        }
+    }
+}
